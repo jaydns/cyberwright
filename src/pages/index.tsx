c03cb4d934003e233e218bf374d8a5b72fa5aa7a
pradhamk <email@pradhamk.com>
2024-09-28 15:47:36 -0400

    dwakjd

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,16 @@
+import Editor from '@monaco-editor/react';
+
 export default function Home() {
   return (
-    <div>
+    <div className='hidden'>
+      <div className='flex w-20 relative'>
+        <h1>test</h1>
+      </div>
+      <div className='flex w-20 relative'>
+        <h1>test</h1>
+      </div>
+
+      
     </div>
   );
 }
