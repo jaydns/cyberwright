c4a95413ca85c92fdd2b469ea3b97912b1030af0
pradhamk <email@pradhamk.com>
2024-09-28 16:23:47 -0400

    dwkjadij

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,10 +2,18 @@ import Editor from '@monaco-editor/react';
 
 export default function Home() {
   return (
-    <div>
-
-
-      
+    <div className='flex flex-row'>
+      <div className='flex w-1/5 bg-transparent'>
+        <h1>test</h1>
+      </div>
+      <Editor 
+        height="100vh" 
+        defaultLanguage="javascript" 
+        defaultValue="// some comment"
+        theme='vs-dark'
+        className='w-4/5'
+        options={{ readOnly: true }}
+      />
     </div>
   );
 }
