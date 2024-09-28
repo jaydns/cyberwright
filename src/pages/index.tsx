97ffa74df97e0eed528530e6f22087325befb603
pradhamk <email@pradhamk.com>
2024-09-28 15:51:05 -0400

    fixed tw

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,13 +2,8 @@ import Editor from '@monaco-editor/react';
 
 export default function Home() {
   return (
-    <div className='hidden'>
-      <div className='flex w-20 relative'>
-        <h1>test</h1>
-      </div>
-      <div className='flex w-20 relative'>
-        <h1>test</h1>
-      </div>
+    <div>
+
 
       
     </div>
