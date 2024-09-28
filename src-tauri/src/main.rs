64d2a1017affc431dcbd27d2f9d2c6949bae2af0
pradhamk <email@pradhamk.com>
2024-09-28 16:16:09 -0400

    rust

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,8 +1,24 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use window_vibrancy::{apply_blur, apply_vibrancy, NSVisualEffectMaterial};
+use tauri::Manager;
+
 fn main() {
   tauri::Builder::default()
+    .setup(|app| {
+        let window = app.get_window("main").unwrap();
+
+        #[cfg(target_os = "macos")]
+        apply_vibrancy(&window, NSVisualEffectMaterial::Sidebar, None, None)
+            .expect("Unsupported platform! 'apply_vibrancy' is only supported on macOS");
+
+        #[cfg(target_os = "windows")]
+        apply_acrylic(&window, Some((18, 18, 18, 125)))
+            .expect("Unsupported platform! 'apply_blur' is only supported on Windows");
+
+        Ok(())
+    })
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
