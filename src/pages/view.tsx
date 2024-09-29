5aad33bea3062700f03eea1e8414e550ef59a30a
Jacob Chen <nerfdartstorm@gmail.com>
2024-09-28 20:30:01 -0400

    New  sidebar

--- src/pages/view.tsx
+++ src/pages/view.tsx
@@ -1,32 +1,31 @@
 import Editor from "@monaco-editor/react";
 import { ChevronDown } from "lucide-react";
 import React, { useState } from 'react';
+import { Tree, Group } from '@mantine/core';
+import { data } from './data';
 
 
 export default function Landing() {
-  var [isRotated, updateIsRotated] = useState(0);
-  const handleClick = () => {
-    updateIsRotated(isRotated + 1);
-  };
+
   return (
     <div className="flex flex-row h-screen">
       <div className="flex w-1/5 bg-transparent flex-col pl-2 pt-1 pr-2">
-        <div className="text-lg border-b-2 border-neutral-450 pl-1">
-          <div className="flex justify-between">
-            <h1>Folder Name</h1> 
-            <div className="pt-1">
-              <ChevronDown onClick={handleClick}  className={`flex size-5 cursor-pointer hover:border hover:border-white rounded-sm ${isRotated % 2 === 0 ? 'rotate-90' : 'rotate-0'}`} ></ChevronDown>
-            </div> 
-          </div>
-        </div>
-        <div className="pl-3">
-          <div className="flex justify-between">
-            <h1>test</h1>
-            <div className=" text-red-700 pr-2">
-              <h1>1</h1>
-            </div>
-          </div>
-        </div>
+      <Tree
+      data={data}
+      levelOffset={23}
+      renderNode={({ node, expanded, hasChildren, elementProps }) => (
+        <Group gap={5} {...elementProps}>
+          {hasChildren && (
+            <ChevronDown
+              size={18}
+              style={{ transform: expanded ? 'rotate(180deg)' : 'rotate(0deg)' }}
+            />
+          )}
+
+          <span>{node.label}</span>
+        </Group>
+      )}
+    />
       </div>
       <div className="w-4/5 h-screen flex flex-col">
         <Editor
